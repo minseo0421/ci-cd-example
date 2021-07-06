e40525192f19c690141627a49e7ca3c40fd7d7b2
minseo0421 <k4496k@naver.com>
2021-07-06 17:44:09 +0900

    change container_check.sh

--- container_check.sh
+++ container_check.sh
@@ -7,8 +7,8 @@ if [ -n "$NGINX_CONTAINER_ID" ];
     echo "nginx container exist"
     docker stop $NGINX_CONTAINER_ID
     docker rm $NGINX_CONTAINER_ID
-    docker run -d -p 80:80 --name nginx nginx:develop
+    docker run -d -p 80:80 --name nginx k4496k/nginx:main
   else
     echo "nginx container not exist"
-    docker run -d -p 80:80 --name nginx nginx:develop
+    docker run -d -p 80:80 --name nginx k4496k/nginx:develop
 fi
